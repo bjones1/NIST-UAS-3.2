680e082f226c2348d851821bed4b79828c530ba2
bjones1 <bjones@ece.msstate.edu>
2022-04-25 16:36:26 -0500

    Fix: Correct production files.
    Signed-off-by: bjones1 <bjones@ece.msstate.edu>

--- production/install.sh
+++ production/install.sh
@@ -3,12 +3,13 @@
 # ***************************************************************
 # |docname| - Install files and set up the Pi for production mode
 # ***************************************************************
-# This must be run from the directory containing this file. It assumes that `../install-prep.sh` has already been executed.
+# This must be run from the directory containing this file. It assumes that `../install-prep.sh` has already been executed. It somewhat follows the `official Pi docs <https://www.raspberrypi.com/documentation/computers/configuration.html#setting-up-a-routed-wireless-access-point>`_.
 #
 # Set up networking.
 sudo apt install -y dnsmasq hostapd
-sudo cp -r production/files/* /
+sudo cp -r files/* /
 sudo rfkill unblock wlan
+sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
 
 # Reboot for changes to take effect.
